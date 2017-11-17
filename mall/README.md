572b803ac2c3e1191e2e5025cf2f6af4b9f7ea7b
Risatoar <risatoar@163.com>
2017-11-17 17:58:42 +0800

    11/17
    update

--- mall/README.md
+++ mall/README.md
@@ -1,21 +1,21 @@
-# auctionweb
-
-> vue2.0 重构拍卖信息网页
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# mall 
+ 
+> 用vue2.0构建的简单商城系统 
+ 
+## Build Setup 
+ 
+``` bash 
+# install dependencies 
+npm install 
+ 
+# serve with hot reload at localhost:8080 
+npm run dev 
+ 
+# build for production with minification 
+npm run build 
+ 
+# build for production and view the bundle analyzer report 
+npm run build --report 
+``` 
+ 
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader). 
--- mall/src/views/GoodsList.vue
+++ mall/src/views/GoodsList.vue
@@ -10,25 +10,16 @@
 		      <span class="sortby">Sort by:</span>
 		      <a href="javascript:void(0)" class="default cur">Default</a>
 		      <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-		      <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+		      <a href="javascript:void(0)" class="filterby stopPop" @click="showFilter">Filter by</a>
 		    </div>
 		    <div class="accessory-result">
 		      <!-- filter -->
-		      <div class="filter stopPop" id="filter">
+		      <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
 		        <dl class="filter-price">
 		          <dt>Price:</dt>
-		          <dd><a href="javascript:void(0)">All</a></dd>
-		          <dd>
-		            <a href="javascript:void(0)">0 - 100</a>
-		          </dd>
-		          <dd>
-		            <a href="javascript:void(0)">100 - 500</a>
-		          </dd>
-		          <dd>
-		            <a href="javascript:void(0)">500 - 1000</a>
-		          </dd>
-		          <dd>
-		            <a href="javascript:void(0)">1000 - 2000</a>
+		          <dd><a href="javascript:void(0)" :class="{'cur':priceChicked == 'all'}" @click="priceChicked ='all'">All</a></dd>
+		          <dd  v-for="(price,index) in priceFilter" @click="priceChicked = index">
+		            <a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{'cur':priceChicked==index}">{{ price.startPrice }} - {{ price.endPrice }}</a>
 		          </dd>
 		        </dl>
 		      </div>
@@ -37,49 +28,15 @@
 		      <div class="accessory-list-wrap">
 		        <div class="accessory-list col-4">
 		          <ul>
-		            <li>
-		              <div class="pic">
-		                <a href="#"><img src="static/1.jpg" alt=""></a>
-		              </div>
+		            <li v-for="(item,index) in goodsList">
+		            	<div class="pic">
+		            	<a href="#">
+		            		<img v-lazy="'/static/'+item.productImg" alt="">
+		            	</a>
+		            	</div>
 		              <div class="main">
-		                <div class="name">XX</div>
-		                <div class="price">999</div>
-		                <div class="btn-area">
-		                  <a href="javascript:;" class="btn btn--m">加入购物车</a>
-		                </div>
-		              </div>
-		            </li>
-		            <li>
-		              <div class="pic">
-		                <a href="#"><img src="static/2.jpg" alt=""></a>
-		              </div>
-		              <div class="main">
-		                <div class="name">XX</div>
-		                <div class="price">1000</div>
-		                <div class="btn-area">
-		                  <a href="javascript:;" class="btn btn--m">加入购物车</a>
-		                </div>
-		              </div>
-		            </li>
-		            <li>
-		              <div class="pic">
-		                <a href="#"><img src="static/3.jpg" alt=""></a>
-		              </div>
-		              <div class="main">
-		                <div class="name">XX</div>
-		                <div class="price">500</div>
-		                <div class="btn-area">
-		                  <a href="javascript:;" class="btn btn--m">加入购物车</a>
-		                </div>
-		              </div>
-		            </li>
-		            <li>
-		              <div class="pic">
-		                <a href="#"><img src="static/4.jpg" alt=""></a>
-		              </div>
-		              <div class="main">
-		                <div class="name">XX</div>
-		                <div class="price">2499</div>
+		                <div class="name">{{ item.productName }}</div>
+		                <div class="price">{{ item.productPrice }}</div>
 		                <div class="btn-area">
 		                  <a href="javascript:;" class="btn btn--m">加入购物车</a>
 		                </div>
@@ -91,6 +48,7 @@
 		    </div>
 		  </div>
 		</div>
+		<div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
 		<nav-footer></nav-footer>
 	</div>
 </template>
@@ -112,7 +70,23 @@
 		},
 		data() {
 			return {
-				goodsList: []
+				goodsList: [],
+				priceFilter: [
+				{
+					startPrice: '0.00',
+					endPrice: '500.00'
+				},
+				{
+					startPrice: '500.00',
+					endPrice: '1000.00'
+				},
+				{
+					startPrice: '1000.00',
+					endPrice: '2000.00'
+				}],
+				priceChicked: 'all',
+				filterBy: false,
+				overLayFlag: false
 			}
 		},
 		mounted: function () {
@@ -124,6 +98,18 @@
 					var res = result.data;
 					this.goodsList = res.result;
 				});
+			},
+			showFilter() {
+				this.filterBy = true;
+				this.overLayFlag= true
+			},
+			closePop() {
+				this.filterBy = false;
+				this.overLayFlag= false
+			},
+			setPriceFilter(index) {
+				this.priceChicked = index;
+				this.closePop();
 			}
 		}
 	}
